2b02d844b1063874bd5584ae6168a371b86b0267
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-24 23:43:59 -0400

    refactor: remove stylisticDisabledRule for improved configuration clarity

--- src/module/stylistic/stylistic.ts
+++ src/module/stylistic/stylistic.ts
@@ -1,21 +1,15 @@
-import { stylisticDisabledRule } from '@module/stylistic/rule/stylistic_disabled_rule';
+import { PACKAGE_NAME } from '@global/constant/package_name';
 import { stylisticGeneralRule } from '@module/stylistic/rule/stylistic_general_rule';
 import pluginStylistic from '@stylistic/eslint-plugin';
 
 import type { Linter } from 'eslint';
 
-const stylistic: Linter.Config =
-{
-  name: 'stylistic',
-  plugins:
-  {
+const stylistic: Linter.Config = {
+  name: `${PACKAGE_NAME}/stylistic`,
+  plugins: {
     style: pluginStylistic
   },
-  rules:
-  {
-    ...stylisticGeneralRule,
-    ...stylisticDisabledRule
-  }
+  rules: stylisticGeneralRule
 };
 
 export { stylistic };
