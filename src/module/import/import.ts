920570633fe0654771afcfebb65e64c41c74ba78
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-24 23:42:06 -0400

    refactor: restructure import configuration for improved clarity and maintainability

--- src/module/import/import.ts
+++ src/module/import/import.ts
@@ -1,25 +1,20 @@
-import { importDisabledRule } from '@module/import/rule/import_disabled_rule';
-import { importGeneralRule } from '@module/import/rule/import_general_rule';
-import { importOverride } from '@module/import/rule/import_override';
 import pluginImport from 'eslint-plugin-import-x';
 
 import type { Linter } from 'eslint';
+import { PACKAGE_NAME } from '@global/constant/package_name';
 
-const imports: Linter.Config[] =
-[
-  {
-    name: 'import',
-    plugins:
-    {
-      import: pluginImport as unknown as Plugin
-    },
-    rules:
-    {
-      ...importGeneralRule,
-      ...importDisabledRule
-    }
-  },
-  ...importOverride
-];
+const imports: Linter.Config = {
+  name: `${PACKAGE_NAME}/import`,
+  plugins: { import: pluginImport as unknown as Plugin },
+  rules: {
+    'import/group-exports': 'error',
+    'import/no-namespace': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-amd': 'error',
+    'import/no-webpack-loader-syntax': 'error',
+    'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+    'import/no-anonymous-default-export': ['error', { allowCallExpression: false }]
+  }
+};
 
 export { imports };
