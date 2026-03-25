313df3f09847101ae60f064ff8aa85dfb5761485
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-24 23:40:43 -0400

    fix: update tsup configuration for improved build settings and output format

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: [ 'src/index.ts' ],
-  shims: true
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  clean: true,
+  minify: true
 });
