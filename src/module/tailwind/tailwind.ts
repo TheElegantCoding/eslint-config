e22b039cdffde7e63ce807c79de28f2d16c9c967
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-24 23:44:08 -0400

    refactor: remove unused rules for improved clarity and maintainability

--- src/module/tailwind/tailwind.ts
+++ src/module/tailwind/tailwind.ts
@@ -1,30 +1,19 @@
-import { tailwindDisabledRule } from '@module/tailwind/rule/tailwind_disabled_rule';
-import { tailwindGeneralRule } from '@module/tailwind/rule/tailwind_general_rule';
 import pluginTailwind from 'eslint-plugin-tailwindcss';
 
 import type { Linter } from 'eslint';
 
-const tailwind: Linter.Config =
-{
-  languageOptions:
-  {
-    parserOptions:
-    {
-      ecmaFeatures:
-      {
-        jsx: true
-      }
-    }
+const tailwind: Linter.Config = {
+  languageOptions: {
+    parserOptions: { ecmaFeatures: { jsx: true } }
   },
   name: 'tailwind',
-  plugins:
-  {
-    tailwind: pluginTailwind
-  },
-  rules:
-  {
-    ...tailwindGeneralRule,
-    ...tailwindDisabledRule
+  plugins: { tailwind: pluginTailwind },
+  rules: {
+    'tailwind/classnames-order': 'error',
+    'tailwind/enforces-negative-arbitrary-values': 'error',
+    'tailwind/enforces-shorthand': 'error',
+    'tailwind/migration-from-tailwind-2': 'error',
+    'tailwind/no-contradicting-classname': 'error'
   }
 };
 
