2588dae3248cd7632266377d85d23285468b7154
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-25 15:13:54 -0400

    refactor: move PACKAGE_NAME import to the top for better readability

--- src/module/yml/yml.ts
+++ src/module/yml/yml.ts
@@ -1,10 +1,10 @@
 /* eslint-disable import/no-namespace */
+import { PACKAGE_NAME } from '@global/constant/package_name';
 import { ymlGeneralRule } from '@module/yml/rule/yml_general_rule';
 import pluginYml from 'eslint-plugin-yml';
 import * as yamlParser from 'yaml-eslint-parser';
 
 import type { Linter } from 'eslint';
-import { PACKAGE_NAME } from '@global/constant/package_name';
 
 const yml: Linter.Config = {
   files: ['**/*.y?(a)ml'],
