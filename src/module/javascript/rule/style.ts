c50d4d5f0bc3e66fb7ff87c71e22d5b21af69c55
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-24 23:42:45 -0400

    refactor: improve formatting consistency in style rules for better readability

--- src/module/javascript/rule/style.ts
+++ src/module/javascript/rule/style.ts
@@ -1,39 +1,26 @@
 import type { Linter } from 'eslint';
 
-const style: Linter.RulesRecord =
-{
-  'arrow-body-style':
-  [
+const style: Linter.RulesRecord = {
+  'arrow-body-style': [
     'error',
     'as-needed',
     { requireReturnForObjectLiteral: false }
   ],
-  camelcase:
-  [
-    'error',
-    {
-      ignoreDestructuring: false,
-      properties: 'never'
-    }
-  ],
-  curly: [ 'error', 'all' ],
+  camelcase: ['error', { ignoreDestructuring: false, properties: 'never' }],
+  curly: ['error', 'all'],
   'dot-notation': 'error',
-  'func-style': [ 'error', 'expression' ],
-  'new-cap':
-  [
-    'error',
-    {
-      capIsNew: false,
-      capIsNewExceptions: [
-        'Immutable.Map',
-        'Immutable.Set',
-        'Immutable.List'
-      ],
-      newIsCap: true,
-      newIsCapExceptions: []
-    }
-  ],
-  'unicode-bom': [ 'error', 'never' ]
+  'func-style': ['error', 'expression'],
+  'new-cap': ['error', {
+    capIsNew: false,
+    capIsNewExceptions: [
+      'Immutable.Map',
+      'Immutable.Set',
+      'Immutable.List'
+    ],
+    newIsCap: true,
+    newIsCapExceptions: []
+  }],
+  'unicode-bom': ['error', 'never']
 };
 
 export { style };
