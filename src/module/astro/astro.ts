11f552f4b270d121eff4d0e878279ca01a34cf79
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-24 23:41:35 -0400

    refactor: simplify astro configuration and improve rule organization

--- src/module/astro/astro.ts
+++ src/module/astro/astro.ts
@@ -1,77 +1,29 @@
-import { astroDisabledRule } from '@module/astro/rule/astro_disabled_rule';
-import { astroGeneralRule } from '@module/astro/rule/astro_general_rule';
-import { astroOverride } from '@module/astro/rule/astro_override';
 import parserTs from '@typescript-eslint/parser';
 import parserAstro from 'astro-eslint-parser';
 import pluginAstro from 'eslint-plugin-astro';
 import globals from 'globals';
 
-import type { Linter } from 'eslint';
+import { astroGeneralRule } from './rule/astro_general_rule';
+import { astroOverride } from './rule/astro_override';
 
-const astro: Linter.Config[] =
-[
-  {
-    name: 'astro',
-    plugins:
-    {
-      astro: pluginAstro
-    }
-  },
-  {
-    files: [ '**/*.astro', '*.astro' ],
-    languageOptions:
-    {
-      globals:
-      {
-        ...globals.node,
-        Astro: false,
-        Fragment: false
-      },
-      parser: parserAstro,
-      parserOptions:
-      {
-        extraFileExtensions: [ '.astro' ],
-        parser: parserTs
-      },
-      sourceType: 'module'
+import type { Linter } from 'eslint';
+import { PACKAGE_NAME } from '@global/constant/package_name';
+
+const astro: Linter.Config[] = [{
+  name: `${PACKAGE_NAME}/astro`,
+  plugins: { astro: pluginAstro },
+  files: ['**/*.astro'],
+  languageOptions: {
+    globals: {
+      ...globals.node,
+      ...globals.browser,
+      Astro: 'readonly',
+      Fragment: 'readonly'
     },
-    name: 'astro:setup',
-    processor: 'astro/client-side-ts',
-    rules:
-    {
-      ...astroGeneralRule,
-      ...astroDisabledRule
-    }
+    parser: parserAstro,
+    parserOptions: { extraFileExtensions: ['.astro'], parser: parserTs }
   },
-  {
-    files: [ '**/*.astro/*.js', '*.astro/*.js' ],
-    languageOptions:
-    {
-      globals:
-      {
-        ...globals.browser
-      },
-      sourceType: 'module'
-    },
-    name: 'astro:javascript'
-  },
-  {
-    files: [ '**/*.astro/*.ts', '*.astro/*.ts' ],
-    languageOptions:
-    {
-      globals: {
-        ...globals.browser
-      },
-      parser: parserTs,
-      parserOptions:
-      {
-        project: null
-      },
-      sourceType: 'module'
-    },
-    name: 'astro:typescript'
-  },
-  ...astroOverride
-];
+  rules: astroGeneralRule
+}, ...astroOverride];
 
 export { astro };
