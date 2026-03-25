185f88be5edf76319e97180c123363795c29b736
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-24 23:44:55 -0400

    refactor: improve formatting and organization of typescriptStylisticTypeChecked rules for clarity

--- src/module/typescript/rule/typescript_stylistyc_type_checked.ts
+++ src/module/typescript/rule/typescript_stylistyc_type_checked.ts
@@ -2,12 +2,15 @@ import { style } from '@module/javascript/rule/style';
 
 import type { Linter } from 'eslint';
 
-const typescriptStylisticTypeChecked: Linter.RulesRecord =
-{
+const typescriptStylisticTypeChecked: Linter.RulesRecord = {
   'dot-notation': 'off',
   'ts/dot-notation': style['dot-notation'] ?? 'off',
   'ts/non-nullable-type-assertion-style': 'error',
-  'ts/prefer-nullish-coalescing': [ 'error', { ignoreTernaryTests: false } ],
+  'ts/prefer-nullish-coalescing': ['error', {
+    ignoreConditionalTests: true,
+    ignoreTernaryTests: false,
+    ignoreMixedLogicalExpressions: true
+  }],
   'ts/prefer-optional-chain': 'error',
   'ts/prefer-string-starts-ends-with': 'error'
 };
