9ec394a05a2ee857d0b9546d01b4a6b99c8d689d
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-25 21:36:48 -0400

    refactor: update perfectionist name to include PACKAGE_NAME

--- src/module/perfectionist/perfectionist.ts
+++ src/module/perfectionist/perfectionist.ts
@@ -1,10 +1,11 @@
+import { PACKAGE_NAME } from '@global/constant/package_name';
 import { perfectionistGeneralRule } from '@module/perfectionist/rule/perfectionist_general_rule';
 import pluginPerfectionist from 'eslint-plugin-perfectionist';
 
 import type { Linter } from 'eslint';
 
 const perfectionist: Linter.Config = {
-  name: 'perfectionist',
+  name: `${PACKAGE_NAME}/perfectionist`,
   plugins: { perfectionist: pluginPerfectionist as unknown as Plugin },
   rules: perfectionistGeneralRule
 };
